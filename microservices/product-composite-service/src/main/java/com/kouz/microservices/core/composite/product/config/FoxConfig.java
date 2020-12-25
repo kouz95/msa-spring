793539c67c191055f9ba503f24f160e6298217b1
kouz <lastpuzly@gmail.com>
2020-12-25 15:25:49 +0900

    feat: add api docs(swagger)

---
+++ microservices/product-composite-service/src/main/java/com/kouz/microservices/core/composite/product/config/FoxConfig.java
@@ -0,0 +1,51 @@
+package com.kouz.microservices.core.composite.product.config;
+
+import static java.util.Collections.*;
+import static org.springframework.http.HttpMethod.*;
+import static springfox.documentation.builders.RequestHandlerSelectors.*;
+import static springfox.documentation.spi.DocumentationType.*;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class FoxConfig {
+    @Value("${api.common.version}")           String apiVersion;
+    @Value("${api.common.title}")             String apiTitle;
+    @Value("${api.common.description}")       String apiDescription;
+    @Value("${api.common.termsOfServiceUrl}") String apiTermsOfServiceUrl;
+    @Value("${api.common.license}")           String apiLicense;
+    @Value("${api.common.licenseUrl}")        String apiLicenseUrl;
+    @Value("${api.common.contact.name}")      String apiContactName;
+    @Value("${api.common.contact.url}")       String apiContactUrl;
+    @Value("${api.common.contact.email}")     String apiContactEmail;
+
+    @Bean
+    public Docket apiDocumentation() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(basePackage("com.kouz.microservices.core.composite.product"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalResponses(GET, emptyList())
+                .apiInfo(new ApiInfo(
+                        apiTitle,
+                        apiDescription,
+                        apiVersion,
+                        apiTermsOfServiceUrl,
+                        new Contact(apiContactName, apiContactUrl, apiContactEmail),
+                        apiLicense,
+                        apiLicenseUrl,
+                        emptyList()
+                ));
+
+    }
+}
